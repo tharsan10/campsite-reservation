86d53a2825a3009c71496a006cd5c23a346603a7
Piri Kengkarasa <PKengkarasa@ad.savaria.com>
2021-04-23 13:28:21 -0400

    first commit spring boot project setup

---
+++ src/main/java/com/capmsite/api/CampsiteReservationApplication.java
@@ -0,0 +1,13 @@
+package com.capmsite.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampsiteReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampsiteReservationApplication.class, args);
+	}
+
+}
